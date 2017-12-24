5602238b842c10e3f33220225c9a58d3d2d4ec78
liujunyang <liujunyangscript@gmail.com>
2017-12-24 11:32:56 +0800

    set arguments

--- index.js
+++ index.js
@@ -6,7 +6,12 @@
  */
 
 // 后面花括号是函数参数的默认值，前面花括号内的等号是解构赋值的默认值，是两码事
-module.exports = ({moduleName = ''} = {}) => {
+module.exports = ({
+	moduleName = '',
+	cwd = process.cwd(),
+	registry = 'https://registry.npmjs.org/',
+	beforeInstall = () => {}
+} = {}) => {
 	return new Promise(resolve => {
 		resolve('a')
 	})
